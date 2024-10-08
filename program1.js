341930eb4a99568812581c67f458b0056e2707e6
YogeshKumar805 <bosyogeshkumar@gmail.com>
2024-10-08 15:43:29 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+        const mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+
+        for (const c of s) {
+            if (Object.values(mapping).includes(c)) {
+                stack.push(c);
+            } else if (mapping.hasOwnProperty(c)) {
+                if (!stack.length || mapping[c] !== stack.pop()) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.length === 0; 
 };
 
 module.exports = { isValid };
